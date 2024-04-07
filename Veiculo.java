73dbc5e7715ff44aebcab96618e8e91447aa8c3e
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-06 21:46:43 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -5,14 +5,56 @@ public class Veiculo {
     protected int ano;
     protected String placa;
 
-    public Veiculo(String modelo, String cor, String marca, int ano, String placa){
+    protected boolean ligado;
+    protected boolean ocupado;
+
+    public Veiculo(String modelo, String cor, String marca, int ano, String placa, boolean ligado, boolean ocupado) {
         this.modelo = modelo;
         this.cor = cor;
         this.marca = marca;
         this.ano = ano;
         this.placa = placa;
+        this.ligado = false;
+        this.ocupado = false;
 
     }
+
+    public void ligar() {
+        setLigado(true);
+    }
+
+    public void desligar() {
+        setLigado(false);
+    }
+
+    public void acelerar() {
+        System.out.println("VRUMM!!!ESTOY ACELERANDO!");
+    }
+
+    public void frear() {
+        System.out.println("ESTOY FREANDO!");
+    }
+
+    public void buzinar() {
+        System.out.println("BEEP BEEP BEEP");
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public boolean isOcupado() {
+        return ocupado ;
+    }
+
 
 }
 
