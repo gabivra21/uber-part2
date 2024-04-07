4a6b9c84daa5840d0547e427f7789686cde03c0b
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-06 21:36:45 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -2,13 +2,10 @@ public class Carro extends Veiculo{
 
 
 
-    public Carro (String modelo, int ano, String cor, String placa, String marca){
-       super(modelo, cor, marca, ano, placa);
+    public Carro (String modelo, String cor, String marca, int ano, String placa, boolean ligado, boolean ocupado){
+       super(modelo, cor, marca, ano, placa, ligado, ocupado);
     }
 
-    /*public Carro(){
-        this("Kicks",2018,"preto","BEE2114","Nissan");
-    }*/
 
     public String getModelo(){
         return modelo;
@@ -22,24 +19,7 @@ public class Carro extends Veiculo{
         this.cor = cor;
     }
 
-    public void ligar(){
 
-    }
-    public void desligar(){
-
-    }
-    public void acelerar(){
-
-    }
-    public void frear(){
-
-    }
-    public void buzinar(){
 
     }
 
-
-
-
-
-}
