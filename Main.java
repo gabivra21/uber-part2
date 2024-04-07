5fb299e548060c5a5b4810eb3005a24f3052c565
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-06 21:41:40 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,14 +11,13 @@ public class Main {
         Motorista motorista1 = new Motorista("Claudia", "Cnh Ear", 4.0f, "claudinha@gmail.com", "solamarelo", "Curitiba", "viagens", 992341569);
         Motorista motorista2 = new Motorista("Kecio", "Cnh Ear", 3.9f, "kecio.silva@hotmail.com", "yolanda78", "Maringá", "viagens e entregas", 976543267);
 
-        Carro carro1 = new Carro("Clio", 2009, "Cinza", "AUU0902", "Renault");
-        Carro carro2 = new Carro("Kicks", 2018, "preto", "BEE2114", "Nissan");
+        Carro carro1 = new Carro("Clio", "Preto", "Renault", 2018, "AUU0902", false, false);
+        Carro carro2 = new Carro("Kicks", "Cinza", "Renault", 2014, "BEE2114", false, false);
 
-        Moto moto1 = new Moto("Biz", "vermelho", "Honda", 2014, "PIA45BB");
-        Moto moto2 = new Moto("Harley", "Verde", "Harley-Davidson", 2018, "OOI-9876");
+        Moto moto1 = new Moto("Biz", "vermelho", "Honda", 2014, "PIA45BB", false, false);
+        Moto moto2 = new Moto("Harley", "Verde", "Harley-Davidson", 2018, "OOI-9876",false ,false);
 
         Menu menu = new Menu();
-
 
         Pagamento pagamento1 = new Pagamento(true, false, false, false, false, 89, 6);
         Pagamento pagamento2 = new Pagamento();
@@ -45,10 +44,6 @@ public class Main {
         System.out.println("Sua cor é: " + moto1.cor);
         System.out.println("Seu modelo é: " + moto1.modelo);*/
 
-
-        //Scanner scanner = new Scanner(System.in);
-        //int numero = scanner.nextInt();
-
         System.out.println("------MENUUU------");
         System.out.println("1-Imprimir informações do passageiro:");
         System.out.println("2-Imprimir informações do motorista:");
@@ -56,12 +51,52 @@ public class Main {
         System.out.println("4-Cancelar corrida");
         System.out.println("0 - SAIR");
 
-        //Scanner scanner = new Scanner(System.in);
-        //int numero = scanner.nextInt();
+
 
         System.out.println("Escolha uma opção:");
         Scanner scanner = new Scanner(System.in);
         int numero = scanner.nextInt();
+
+        if (numero == 0){
+            System.exit(0);
+        }
+
+        if (numero == 3){
+            if(carro1.ocupado == false) {
+                System.out.println("O carro 1 foi escolhido!");
+                System.out.println("Vamos verificar se ele esta ocupado!");
+                System.out.println(carro1.ocupado);
+                System.out.println("Está desocupado!Corrida chamada.");
+                carro1.setOcupado(true);
+                System.out.println("Vamos verificar seu status agora!");
+                System.out.println(carro1.ocupado);
+            }else {
+
+                System.out.println("O carro 1 está ocupado!Mas podemos chamar o carro 2!");
+                System.out.println("Vamos verificar se ele esta ocupado!");
+                System.out.println(carro2.ocupado);
+                System.out.println("Está desocupado!Corrida chamada.");
+                carro2.setOcupado(true);
+                System.out.println("Vamos verificar seu status agora!");
+                System.out.println(carro2.ocupado);
+            }
+        }
+
+        if(numero == 4){
+            System.out.println("Vamos cancelar sua corrida!");
+            System.out.println("Escolha o carro que você chamou (1 ou 2)");
+            Scanner scanner4 = new Scanner(System.in);
+            int numero4 = scanner4.nextInt();
+            if(numero4 == 1){
+                carro1.setOcupado(false);
+                System.out.println("Corrida cancelada! O status do carro agora é "+carro1.ocupado);
+            }else{
+                carro2.setOcupado(false);
+                System.out.println("Corrida cancelada! O status do carro agora é "+carro2.ocupado);
+
+            }
+        }
+
 
         if (numero == 1){
             System.out.println("Escolha o passageiro 1 ou 2: ");
@@ -70,9 +105,25 @@ public class Main {
 
             if(numero2 == 1){
                 menu.imprimirInfoP1();
+            }else {
+                menu.imprimirInfoP2();
             }
 
+            }
+
+        if (numero == 2 ){
+            System.out.println("Escolha o motorista 1 ou 2: ");
+            Scanner scanner2 = new Scanner(System.in);
+            int numero3 = scanner2.nextInt();
+
+            if (numero3==1){
+                menu.imprimirInfoM1();
+            }else{
+                menu.imprimirInfoM2();
+            }
         }
+
+
 
 
 
@@ -83,8 +134,7 @@ public class Main {
 
 
 
-
-
+        
 
 
 
